b7e5db53836fa041a9a751aa99d4564376ed6bd9
Gyanendra Mishra <anomaly.the@gmail.com>
2015-04-27 02:14:30 +0530

    amazon done

--- amzn.py
+++ amzn.py
@@ -10,32 +10,67 @@ socket.setdefaulttimeout(10000)
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-def visitPhones(page):
-	pass
-
-def action(url):
-	print url
-	p = re.compile(ur'class="pagnNext"\n*\s* href="(.*)">\n?\s*<span id="pagnNextString')
+def visitPhones(phone_url, phone_id):
+	result = []
 	headers = {
-    'User-Agent': 'Mozilla/5.0'
-    }	
-	uf = requests.get(url, headers = headers)
-	if uf.status_code == 200 :
-		print uf.encoding
-		print uf.text
-		sys.exit()
-		#good we are getting 200
-		visitPhones(uf.text)
-		match = re.search(p, uf.text)
-		#check if more pages are pressent
-		if match :
-			amazon("http://www.amazon.in"+str(match.group(1)),i+1)
-		else:
-			print "done!"		
-	else :
-		print 'are u connected?'
+	    'User-Agent': 'Mozilla/5.0'
+	    }
+	response = requests.get(phone_url, headers= headers)
+	if response.status_code == 200:
+		name = re.search('<span id="productTitle" class="a-size-large">(.*)</span>', response.text)
+		picture = re.search('" data-old-hires="(.*)"  class=', response.text)
+		desc = re.findall('<li><span class="a-list-item"> (.*)<\/span>', response.text)
+		price = re.search(' <span class="a-size-mini"> <span class="currencyINR">&nbsp;&nbsp;</span>(.*)</span>', response.text)
+		rating = re.search('class="reviewCountTextLinkedHistogram noUnderline" title="(\d*\.?\d*) out of 5 stars">', response.text)
+		warranty = re.search('Warranty Details:<\/strong> (.*)\W*<\/span>', response.text)
+		asin = re.search('asin=([A-Z0-9]*)&quot;',response.text)
+		if asin.group(1) in phone_id :
+			return False
+		else :
+			phone_id.append(asin.group(1))		
+		if name and price and picture:
+			result.append(name.group(1))
+			result.append(picture.group(1))
+			if desc:
+				result.append(desc)
+			else:
+				result.append('Description not available.')
+			result.append(str(price.group(1)).strip())
+			if rating:
+				result.append(rating.group(1))
+			else :
+				result.append('Rating not available!')
+			if warranty:
+				result.append(warranty.group(1))
+			else :
+				result.append('Warranty not available.')
+			return result
+		else :
+			return False
+	else:
+		return False
+
+def action():
+	phone_list = []
+	phone_id = []
+	for x in xrange(1,1000):
+		url = 'http://www.amazon.in/s/ref=sr_pg_'+str(x)+'?fst=as%3Aoff&rh=n%3A976419031%2Cn%3A1389401031%2Cn%3A1389432031%2Cn%3A1805560031%2Ck%3Asamsung%2Cp_89%3ASamsung&page=2&keywords=samsung&ie=UTF8&qid=1430076551'
+		headers = {
+	    'User-Agent': 'Mozilla/5.0'
+	    }	
+		response = requests.get(url, headers = headers)
+		if response.status_code == 200 :
+			p = re.compile('a-text-normal" title=".*" href="(.*)"><h2')
+			match = re.findall(p, response.text)
+			if match :
+				for phone in match:
+					data = visitPhones(phone, phone_id)
+					if data:
+						print data
+						phone_list.append(phone)
+		else :
+			print 'Done!/Bad connectivity!'
 
 def amazon():
-	print 'Enter Url'
-	url = raw_input()
-	action(url)
+	action()
+action()	
